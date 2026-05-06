7e0e0b2a34cdf8f5452d53213929e7f26756848c
YoHgo0 <elason4ever@gmail.com>
2026-05-06 16:26:09 -0400

    pinecone setup

--- chatbot.py
+++ chatbot.py
@@ -5,16 +5,16 @@ import tiktoken
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone, ServerlessSpec
+import langchain_pinecone
+
 
 load_dotenv()
 
-
-# Api set up and test
+# Api keys setup
 client = OpenAI(api_key=os.getenv(key='OPENAI_API_KEY'))
-response = client.responses.create(
-    model='gpt-5',
-    input='tell me how human brain works(within 20 words)'
-)
+pc = Pinecone(api_key=os.getenv(key='PINECONE_API_KEY'))
 
 # load the documents
 loader = PyPDFLoader("/Users/yuguo/Downloads/House-owner-wordings.pdf")
@@ -27,10 +27,35 @@ splitter = RecursiveCharacterTextSplitter(
 )
 chunks = splitter.split_documents(pages)
 
-# Verify
+# verify
 print(f"Pages loaded: {len(pages)}")
 print(f"Chunks created: {len(chunks)}")
 
-encoding = tiktoken.encoding_for_model('text-embedding-3-small')
-tokens = encoding.encode(" the")
+# langchain openai embedding
+'''response = client.embeddings.create(
+    input=chunks[0].page_content,
+    model="text-embedding-3-small"
+)
+print(f"Number of chunks: {len(chunks)}")'''
+
+embedding = OpenAIEmbeddings(
+    model='text-embedding-3-small',
+)
+chunks_text = [chunk.page_content for chunk in chunks]
+all_vectors = embedding.embed_documents(texts=chunks_text)
+
+# pinecone index created
+index_name = "rag-chatbot"
+
+if index_name not in [i.name for i in pc.list_indexes()]:
+    pc.create_index(
+        name=index_name,
+        dimension=1536, 
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+        deletion_protection='enabled',
+    )
+    print(f"✅ Created index '{index_name}'")
+else:
+    print(f"ℹ️  Index '{index_name}' already exists")
 
